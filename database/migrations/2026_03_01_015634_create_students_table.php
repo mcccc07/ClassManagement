b3546f9e09924cf03451196ab3f409fc024f78da
Mac <macsavanadal@gmail.com>
2026-03-03 09:22:06 +0800

    Updated Migration Student Schema

--- database/migrations/2026_03_01_015634_create_students_table.php
+++ database/migrations/2026_03_01_015634_create_students_table.php
@@ -12,13 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('students', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            Schema::create('students', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('email')->unique();
+                $table->timestamp('email_verified_at')->nullable();
+                $table->string('password');
+                $table->string('course');
+                $table->string('yearlvl');
+                $table->rememberToken();
+                $table->timestamps();
+            });
         });
     }
 
