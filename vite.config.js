933ae943d8120908e6da7e66f60b2b81c35c116c
Mac <macsavanadal@gmail.com>
2026-03-03 09:37:12 +0800

    Updated ViteConfig

--- vite.config.js
+++ vite.config.js
@@ -11,12 +11,3 @@ export default defineConfig({
         tailwindcss(),
     ],
 });
-
-module.exports = {
-    darkMode: ["class", '[data-theme="synthwave"]'],
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
