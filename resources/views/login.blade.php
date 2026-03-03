e2996c7f337b861491b84315234e004572315810
Mac <macsavanadal@gmail.com>
2026-03-03 15:38:36 +0800

    Feature Added

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -31,7 +31,7 @@
                         <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
                         <input type="password" id="password" name="password" required class="w-full px-4 py-2 mt-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
                     </div>
-                    <div class="flex items-center gap-8">
+                    <div class="flex items-center justify-between">
                         <div class="flex items-center">
                             <input type="checkbox" id="remember" name="remember" class="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded">
                             <label for="remember" class="ml-2 block text-sm text-gray-900">Remember Me</label>
